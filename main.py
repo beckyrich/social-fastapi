0799e516bd64d087e24604e84c457356fb658e38
Becky Richardson <rebecca.s.richardson4@gmail.com>
2024-04-30 12:34:55 -0500

    adding delete post

--- main.py
+++ main.py
@@ -16,8 +16,13 @@ my_posts = [{"title": "title of post 1", "content": "content of post 1", "id": 1
 
 def find_post(id):
     for p in my_posts:
-        if p["id"] == id:
+        if p['id'] == id:
             return p
+        
+def find_index_post(id):
+    for i, p in enumerate(my_posts):
+        if p['id'] == id:
+            return i
 
 
 @app.get("/")
@@ -41,3 +46,16 @@ def get_post(id: int):
     if not post:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"post with id: {id} was not found")
     return {"post_detail": post}
+
+@app.delete("/posts/{id}", status_code=status.HTTP_204_NO_CONTENT)
+def delete_post(id: int):
+    #deleting post
+    # find the index in the array 
+    # my_posts.pop(index)
+    index = find_index_post(id)
+
+    if index == None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"post with id: {id} does not exist")
+
+    my_posts.pop(index)
+    return Response(status_code=status.HTTP_204_NO_CONTENT)
